74fb9b15995fd28338e93f4f6f0b65b279fb32c2
herman <hermanconnor11@gmail.com>
2023-12-26 14:39:42 -0500

    Create function to add an item

--- main.js
+++ main.js
@@ -5,6 +5,33 @@ const initApp = () => {
   const clearItemsBtn = document.getElementById('clear');
   const itemFilter = document.getElementById('filter');
   const formBtn = itemForm.querySelector('button');
+
+  // ADD ITEM
+  function addItem(e) {
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+
+    if (!newItem) return;
+
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    const icon = document.createElement('i');
+
+    button.className = 'remove-item btn-link text-red';
+    icon.className = 'fa-solid fa-xmark';
+
+    button.appendChild(icon);
+    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(button);
+
+    itemList.appendChild(li);
+  }
+
+  /**************************
+   * ::: EVENT LISTENERS :::
+   **************************/
+  itemForm.addEventListener('submit', addItem);
 };
 
 document.addEventListener('DOMContentLoaded', initApp);
