b7c96f651d4b58804b8955ef4a57d20b4d161611
herman <hermanconnor11@gmail.com>
2023-12-25 23:59:44 -0500

    Add initializer function, select DOM elements, and add domcontentloaded event listener

---
+++ main.js
@@ -0,0 +1,10 @@
+const initApp = () => {
+  const itemForm = document.getElementById('item-form');
+  const itemInput = document.getElementById('item-input');
+  const itemList = document.getElementById('item-list');
+  const clearItemsBtn = document.getElementById('clear');
+  const itemFilter = document.getElementById('filter');
+  const formBtn = itemForm.querySelector('button');
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
